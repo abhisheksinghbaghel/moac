a50860f1d2ab11f4dfebf4198670adef63921cb7
Jan Kryl <jan.kryl@mayadata.io>
2021-06-23 10:55:07 +0000

    feat: replace pre-commit by husky and lint-staged
    Get rid of clumsy pre-commit requiring python and use modern husky
    that can be better integrated with JS code base.

--- shell.nix
+++ shell.nix
@@ -7,12 +7,5 @@ mkShell {
     etcd
     nats-server
     nodejs-16_x
-    pre-commit
   ];
-
-  shellHook = ''
-    echo "Run following commands to install pre-commit hooks:"
-    echo "pre-commit install"
-    echo "pre-commit install --hook commit-msg"
-  '';
 }
